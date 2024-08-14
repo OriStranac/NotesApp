4264d4f8c2ac134e1feb0fc532a2636342247824
OriStranac <orio6165@gmail.com>
2024-08-14 17:47:54 +0200

    create modal for delete note

---
+++ notes-app/src/components/Notes/ModalDeleteNote.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="modal is-active p-2">
+  <div class="modal-background"></div>
+  <div class="modal-card" ref="modalCardRef">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Delete Note?</p>
+      <button @click="closeModal" class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+        Are you sure you want to delete this note?
+    </section>
+    <footer class="modal-card-foot is-justify-content-flex-end">
+      <div class="buttons">
+        <button @click="closeModal" class="button">Cancel</button>
+        <button class="button is-danger">Delete</button>
+      </div>
+    </footer>
+  </div>
+</div>
+</template>
+
+<script setup>
+import { onClickOutside } from "@vueuse/core";
+import { ref, onMounted, onUnmounted} from "vue";
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+const closeModal = () => {
+  emit("update:modelValue", false);
+};
+
+const modalCardRef = ref(null);
+
+onClickOutside(modalCardRef, closeModal);
+
+const handleKeyboard = (e) => {
+  if (e.key === "Escape") {
+    closeModal();
+  }
+};
+
+onMounted(() => {
+  document.addEventListener("keyup", handleKeyboard);
+});
+
+onUnmounted(() => {
+  document.removeEventListener("keyup", handleKeyboard);
+});
+
+</script>
