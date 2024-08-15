5d5f20d98b204911f74fc329ac5f0c19c80b0041
OriStranac <orio6165@gmail.com>
2024-08-15 13:45:22 +0200

    add date to note

--- notes-app/src/components/Notes/Note.vue
+++ notes-app/src/components/Notes/Note.vue
@@ -3,8 +3,9 @@
     <div class="card-content">
       <div class="content">
         {{ note.content }}
-        <div class="has-text-right has-text-grey-light mt-2">
-          <small>{{ characterLength }}</small>
+        <div class="columns is-mobile has-text-grey-light mt-2">
+          <small class="column">{{ dateFormatted }}</small>
+          <small class="column has-text-right">{{ characterLength }}</small>
         </div>
       </div>
     </div>
@@ -22,12 +23,14 @@
     <ModalDeleteNote
       v-if="modals.deleteNote"
       v-model="modals.deleteNote"
+      :noteId="note.id"
      />
   </div>
 </template>
 
 <script setup>
 import { computed, reactive } from "vue";
+import { useDateFormat} from '@vueuse/core';
 import { useNotesStore } from "@/stores/storeNotes";
 import ModalDeleteNote from "@/components/Notes/ModalDeleteNote.vue";
 
@@ -45,6 +48,11 @@ const characterLength = computed(() => {
 });
 
 const storeNotes = useNotesStore();
+
+const dateFormatted = computed(() => {
+  let date = new Date(parseInt(props.note.date));
+  return useDateFormat(date, 'MM-DD-YYYY @ HH:mm:ss');
+});
 
 const modals = reactive({
   deleteNote: false,
